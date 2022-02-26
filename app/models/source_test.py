44473491d7218c1069cf9f3f095d1a801ccad5d0
saretotracy <tracysareto@gmail.com>
2022-02-26 19:37:47 +0300

    add source_test case file

---
+++ app/models/source_test.py
@@ -0,0 +1,22 @@
+import unittest
+from models.news import Sources
+
+
+class NewsTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the News class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_source = Sources
+        ('abc-new','ABC News','Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.','"https://abcnews.go.com', "general","us")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Sources))
+
+
+if __name__ == '__main__':
+    unittest.main()
