d81db3f63c0d6bc1b4bdc4db56c7d8e8c5a5c48f
saretotracy <tracysareto@gmail.com>
2022-02-26 18:35:52 +0300

    add secret key

--- app/config.py
+++ app/config.py
@@ -2,8 +2,10 @@ class Config:
     '''
     General configuration parent class
     '''
-
+    NEWS_API_KEY = 'NEWS_API_KEY'
+    SECRET_KEY = 'SECRET_KEY'
     NEWS_API_SOURCE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_API_ARTICLES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     
     pass
 
