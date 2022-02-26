d367511e37f7864cb67654fc8db8cb7d6f76e75f
saretotracy <tracysareto@gmail.com>
2022-02-26 23:14:36 +0300

    Function that gets the json response to our url request using the source id and page size

--- app/request.py
+++ app/request.py
@@ -97,4 +97,25 @@ def process_articles_results(articles_list):
 
 
 
+def get_articles_from_source_selected(source, pageLimitSize):
+    '''
+    Function that gets the json response to our url request using the source id and page size
+    '''
+    get_articles_url = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}&pageSize={}'.format(source,
+                                                                                                      api_key, pageLimitSize)
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+
+        articles_results = None
+
+        if get_articles_response['articles']:
+            articles_results_list = get_articles_response['articles']
+            articles_results = process_articles_results(articles_results_list)
+    return articles_results
+
+
+
+
+
    
